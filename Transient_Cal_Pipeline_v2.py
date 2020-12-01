193f65f1b8a8654e72f91b772ad33f085434f99b
smairs <s.mairs@eaobservatory.org>
2020-12-01 10:37:02 -1000

    Turned the pipeline into an iterative method - so Colton's code can now be run multiple times. New directories will be created to store the data from each alignment iteration and tables will be generated labeled by their alignment iteration number.

--- Transient_Cal_Pipeline_v2.py
+++ Transient_Cal_Pipeline_v2.py
@@ -4,59 +4,75 @@ from jcmt_transient_alignment.create_makemap_script import make_pcor, makemap_in
 import subprocess
 
 regions_to_run = ['DR21C']
+datadirs       = ['DR21C']
 wave = '450'
+XC_alignment_iterations = 1
 
-######################################
-######################################
-######################################
-# STEP 1: Run XC and AC
-######################################
-######################################
-######################################
+# We may wish to run the cross correlation technique to align the images multiple times
+# so this loop works for 1 or more iterations of Colton's codes.
 
-# Run the Cross Correlation and Auto Correlation on the input data
-# This will generate a binary file (pickle) with all the data on
-# The XC and AC fits - effectively giving us alignement and relative 
-# flux cal values.
+for alignment_iteration in np.arange(0,XC_alignment_iterations,1):
 
-make_data_dict(regions=regions_to_run)
+    ######################################
+    ######################################
+    ######################################
+    # STEP 1: Run XC and AC
+    ######################################
+    ######################################
+    ######################################
+    
+    # Run the Cross Correlation and Auto Correlation on the input data
+    # This will generate a binary file (pickle) with all the data on
+    # The XC and AC fits - effectively giving us the first alignment and relative 
+    # flux cal values.
 
-# Harvest the data dictionary created above to make a simple txt table
+    if alignment_iteration == 0:
+        make_data_dict(regions=regions_to_run,datadirs=datadirs,alignment_iteration=alignment_iteration)
+        make_table(regions_to_run,alignment_iteration=alignment_iteration)
+    
+    # If the aligment has previously been run in iteration 0 - we need
+    # to point to the already aligned data! So here, datadirs points
+    # to the CR3 files produced on the first iteration. We also
+    # Keep track of the tables by alignment number.
 
-make_table(regions_to_run)
-
-######################################
-######################################
-######################################
-# STEP 2: Perform Alignment and Rel Flux Cal
-######################################
-######################################
-######################################
-
-# Now perform the alignment and flux calibration based on those numbers
-# This requires re-running the data reduction with newly created pointing
-# correction files and then using kappa's cdiv to apply the Rel FCF
-
-
-# First, create the pointing correction files based on the table for each region - the first file will be skipped
-# Next, create files listing all the raw data for makemap to use - this creates one for each pointing correction file
-# Then write a bash script using Python to perform the makemap call
-# Run makemap
-# Remove all the intermediate files
-
-for i in regions_to_run:
-    make_pcor("tables/HM_{:}.table".format(i))
-    makemap_infiles("tables/HM_{:}.table".format(i),i,wave)
-    create_makemap_script(wave)
-    subprocess.call('makemaps.sh',shell=True)
-    os.system('rm -f *pcor.txt  makemaps.sh *_'+wave+'.txt')
+    else:
+        make_data_dict(regions=regions_to_run,datadirs=[eachregion + '_XCalign_'+str(alignment_iteration_number-1) for eachregion in regions_to_run],alignment_iteration=alignment_iteration)
+        make_table(regions=regions_to_run,alignment_iteration=alignment_iteration)
 
 
-# Next, apply the relative FCF correction using kappa!
+    ######################################
+    ######################################
+    ######################################
+    # STEP 2: Perform Alignment and Rel Flux Cal
+    ######################################
+    ######################################
+    ######################################
+    
+    # Now perform the alignment and flux calibration based on the output table for each region
+    # This requires re-running the data reduction with newly created pointing
+    # correction files and then using kappa's cdiv to apply the Rel FCF
+    
+    
+    # First, create the pointing correction files based on the table for each region - the first file will be skipped
+    # Next, create files listing all the raw data for makemap to use - this creates one for each pointing correction file
+    # Then write a bash script using Python to perform the makemap call
+    # Run makemap
+    # Remove all the intermediate files
 
+    for eachregion in regions_to_run:
+        make_pcor("tables/HM_"+eachregion+"_run_"+str(alignment_iteration)+".table")
+        makemap_infiles("tables/HM_"+eachregion+"_run_"+str(alignment_iteration)+".table",eachregion,wave)
+        create_makemap_script(wave)
+        subprocess.call('makemaps.sh',shell=True)
+        if not os.path.exists(eachregion+'_XCalign_'+str(alignment_iteration_number)):
+            os.system('mkdir '+eachregion+'_XCalign_'+str(alignment_iteration_number))
+        # Apply the relative FCF correction using kappa
 
-# ^^ May need to run the above steps multiple times - so the makemap "out" names might get confusing!
-
+        # Move the newly aligned and flux calibrated files to their own directory
+        os.system('mv *CR3_relcal.sdf '+eachregion+'_XCalign_'+str(alignment_iteration_number))
+        # Remove the intermediate files
+        os.system('rm -f *pcor.txt  makemaps.sh *_'+wave+'.txt')
+        
 
 # Now, with the new files, run the localised point source method of
 # finding the relative flux calibration factors. This will avoid any
