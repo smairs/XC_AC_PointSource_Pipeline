29872301bbbfdcc2ce7df139f66c9832422514d1
smairs <s.mairs@eaobservatory.org>
2020-12-04 12:51:52 -1000

    Now create noise txt files within this code - smooth the data before the point source analysis - a lot of package updates that are now reflected in this code

--- Transient_Cal_Pipeline_v2.py
+++ Transient_Cal_Pipeline_v2.py
@@ -3,6 +3,9 @@ from jcmt_transient_alignment.HighMass_table_gen_EAO import make_table
 from jcmt_transient_alignment.create_makemap_script import make_pcor, makemap_infiles, create_makemap_script
 from jcmt_transient_alignment.applycal import apply_relFCF_AC
 from point_source_cal.make_coadds_metadata_tables import make_coadds_metadata
+from point_source_cal.noisefunctions import make_noise_file
+from point_source_cal.smooth_input_data import smooth_input_data
+from point_source_cal.make_FCFunc_vs_sourceSNR_plots_binsof1_plottogether import make_FCFunc_family_plots
 import subprocess
 import os
 import numpy as np
@@ -84,23 +87,57 @@ for alignment_iteration in np.arange(0,XC_alignment_iterations,1):
         os.system('mv *CR3_ACcal.sdf '+eachregion+'_XCalign_'+str(alignment_iteration))
         # Remove the intermediate files
         os.system('rm -f *pcor.txt  makemaps.sh *_'+wave+'.txt *_CR3.sdf')
-        
+     
+#####
+#####
+#####   
+#####
+# Localised Algorithm Start
+#####
+#####
+#####
+#####
 
-# Now, with the new files, run the localised point source method of
-# finding the relative flux calibration factors. This will avoid any
-# variables and improve the original correction. At the same time,
-# we can get the alignment factors based on the localised method
-# to test the cross correlation alignement went smoothly
+# Get most recent XC/AC iteration
 
-#Once config directory is set up and protocat/diskcat are int he right place....
-#Run:
-#make_coadds_metadata_tables.py
+for eachregion in regions_to_run:
+    allfiles = os.listdir('./')
+    XCalign_dirs = []
+    for i in allfiles:
+        if os.path.isdir(i):
+            if len(i.split('_'))>1:
+                if i.split('_')[-2] == 'XCalign':
+                    if i.split('_')[0] == eachregion:
+                        XCalign_dirs.append(i)
+    align_numbers = []
+    for i in XCalign_dirs:
+        align_numbers.append(int(i.split('_')[-1]))
+    most_recent_XC = eachregion+'_XCalign_'+str(max(align_numbers))
+    
+    # Now, with the new files, run the localised point source method of
+    # finding the relative flux calibration factors. This will avoid any
+    # variables and improve the original correction. At the same time,
+    # we can get the alignment factors based on the localised method
+    # to test the cross correlation alignement went smoothly
+    
+    # Smooth all the data!
 
-for eachregion in regions_to_run
-    make_coadds_metadata(region,wave)
+    smooth_input_data(most_recent_XC,wave)   
+ 
+    make_coadds_metadata(eachregion,most_recent_XC,wave)
 
-# This will give all the files necessary to run make*plottogether*py which uses getfamily
-# Then I will get family members, FCFs, FCFuncs, etc. 
-# Apply to the data.
-# Find a way to run alignment as well to check Colton's Results
+    # Generate noises_wave.txt
+   
+    make_noise_file(sorted(glob.glob(most_recent_XC+'/*'+wave+'*sm.sdf'))) 
+    
+    # This will give all the files necessary to run make*plottogether*py which uses getfamily
+    # and will generate FCF unc family plots as well as a dictionary of datescans and Rel FCFS
+    # -- the dictionary will also include the family member IDs
 
+    make_ 
+
+    # Then I will get family members, FCFs, FCFuncs, etc. 
+    # Apply to the data.
+    # Find a way to run alignment as well to check Colton's Results
+
+    
